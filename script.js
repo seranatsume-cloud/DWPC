2ee5c00d8861f6aebf1a7c9879eba327a2fb7e0c
ShinkaiSeiyaku <seranatsume@gmail.com>
2026-03-27 22:03:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ if (intro) {
   });
 }
 
-// 動画あるときだけ
+// ===== 動画がある時だけ実行 =====
 const video = document.getElementById("introVideo");
 
 if (video) {
@@ -15,3 +15,30 @@ if (video) {
     intro.style.display = "none";
   };
 }
+
+// ===== グリッチ文字 =====
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+document.querySelectorAll(".glitch-text").forEach(el => {
+  el.addEventListener("mouseover", () => {
+    let iteration = 0;
+    const original = el.innerText;
+
+    const interval = setInterval(() => {
+      el.innerText = original
+        .split("")
+        .map((letter, index) => {
+          if (index < iteration) return original[index];
+          return letters[Math.floor(Math.random() * letters.length)];
+        })
+        .join("");
+
+      iteration += 1 / 3;
+
+      if (iteration >= original.length) {
+        clearInterval(interval);
+        el.innerText = original;
+      }
+    }, 30);
+  });
+});
