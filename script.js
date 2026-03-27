1e0c792232f61584d2af29a5b8b400b5562222f1
ShinkaiSeiyaku <seranatsume@gmail.com>
2026-03-27 21:41:54 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
-window.onload = () => {
-  const glitch = document.getElementById("glitch");
+const video = document.getElementById("introVideo");
+const intro = document.getElementById("intro");
 
-  // 起動時ノイズ
-  glitch.style.opacity = 1;
-
-  setTimeout(() => {
-    glitch.style.opacity = 0;
-  }, 100);
-
-  // フラッシュを何回か繰り返す
-  let count = 0;
-  let interval = setInterval(() => {
-    glitch.style.opacity = Math.random();
-
-    count++;
-    if (count > 10) {
-      glitch.style.opacity = 0;
-      clearInterval(interval);
-    }
-  }, 50);
+video.onended = () => {
+  intro.style.display = "none";
 };
